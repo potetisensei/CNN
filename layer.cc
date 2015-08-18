c0cb4266a5a2b60f6a51516da63f3f22d693ad0a
potetisensei <poteticalbee@gmail.com>
2015-08-18 07:55:18 -0700

    remove Layer::CalculateOutputUnits

--- layer.cc
+++
@@ -1,7 +0,0 @@
-#include "layer.h"
-
-void Layer::CalculateOutputUnits(vector<struct Neuron> units) {
-    for (int i=0; i<units.size(); i++) {
-        units[i].z = f_->Calculate(units[i].u, units);
-    }
-}
--- layer.h
+++ layer.h
@@ -9,24 +9,15 @@ using namespace std;
 
 class Layer {
 public:
-    Layer() : calculated_(false), f_(&id_) {}
-    Layer(double rate, ActivationFunction *f) 
-        : calculated_(false), learning_rate_(rate), f_(f) {}
+    Layer() {}
     virtual ~Layer() {}
     virtual void CheckInputUnits(vector<struct Neuron> const &units) { assert(0); } 
     virtual void ArrangeOutputUnits(vector<struct Neuron> &units) { assert(0); }
     virtual void ConnectNeurons(vector<struct Neuron> const &input, vector<struct Neuron> const &output) { assert(0); }
-    virtual void CalculateOutputUnits(vector<struct Neuron> &units);
+    virtual void CalculateOutputUnits(vector<struct Neuron> &units) { assert(0); }
     virtual void Propagate(vector<struct Neuron> const &input, vector<struct Neuron> const &output) { assert(0); }
     virtual void BackPropagate(vector<struct Neuron> const &input, vector<struct Neuron> const &output) { assert(0); }
     virtual void UpdateWeights() { assert(0); }
-/*
-    bool calculated_;
-    DoubleVector2d deltas_; // [sample_idx][neuron_idx] 
-    ActivationFunction *f_;
-public:
-    double learning_rate_;
-    Identity id_;*/
 };
 
 #endif
